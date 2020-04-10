887a64af8dca69f7b15afb3046eb28cd4d5ced45
Andrés Montaña <andimier@gmail.com>
2020-04-10 15:08:47 -0500

    FRONT - add utils folder

---
+++ utils/phpfunctions.php
@@ -0,0 +1,38 @@
+<?php
+    function phpMethods($method, $param) {
+		global $connection;
+
+		if (phpversion() < 6) {
+			switch ($method) {
+				case 'query':
+					return mysql_query($param, $connection);
+					break;
+				case 'error':
+					return mysql_error();
+					break;
+				case 'fetch':
+					return mysql_fetch_array($param);
+					break;
+				case 'num-rows':
+					return mysql_num_rows($param);
+					break;
+			}
+		} 
+		else {
+			switch ($method) {
+				case 'query':
+					return mysqli_query($connection, $param);
+					break;
+				case 'error':
+					return mysqli_error($connection);
+					break;
+				case 'fetch':
+					return mysqli_fetch_array($param);
+					break;
+				case 'num-rows':
+					return mysqli_num_rows($param);
+					break;
+			}
+		}
+    }
+?>
