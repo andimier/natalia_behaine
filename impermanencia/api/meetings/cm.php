9141c6bdeddd29f23a0ff3d0b7ccbdf63b03cabd
Andrés Montaña <andimier@gmail.com>
2020-06-10 17:24:16 -0500

    TIENDA - create class for token fetching

--- impermanencia/api/meetings/cm.php
+++ impermanencia/api/meetings/cm.php
@@ -1,16 +1,64 @@
 <?php
-    $url = "https://zoom.us/oauth/token?grant_type=authorization_code&code=hTAtHmpoQc_q2jGIzNcQoW4kJIsBRhOOQ&redirect_uri=http://www.andimier.com/apitests/meetings/cm.html";
+    class Token {
+
+        function __construct() {
+            $this->redirectUri = "http://www.andimier.com/apitests/meetings/cm.html";
+            $this->keyAndSecret = "WUJISVd3SVRUaktudmpRckhjbDlSdzpEbFh3SEJSREdFWW1ucnNDQUVSSjJ3dTlHNEhCbTIyQQ==";
+            $this->apiHost = "https://zoom.us/oauth/token";
+            $this->localTestUrl = "http://localhost/nataliabehaine/dev-nataliabehaine/impermanencia/api/meetings/cm.php?code=3964982734hbd293bd92";
+        }
+
+        private function getCode() {
+            return $_GET['code'];
+        }
+
+        private function getSlotId() {
+            $userData = null;
+
+            if (isset($_GET['state'])) {
+                $userData = $_GET['state'];
+            }
+
+            return $userData;
+        }
+
+        public function getData() {
+            $queryParams = [
+                "grant_type=authorization_code",
+                "code=".$this->getCode(),
+                "redirect_uri=" . $this->redirectUri
+            ];
+
+            $url = $this->apiHost . "?" . join("&", $queryParams);
     
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-    curl_setopt($ch, CURLOPT_POST, 1); 
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Basic WUJISVd3SVRUaktudmpRckhjbDlSdzpEbFh3SEJSREdFWW1ucnNDQUVSSjJ3dTlHNEhCbTIyQQ==")); 
+            try {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+                curl_setopt($ch, CURLOPT_POST, 1); 
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Basic " . $this->keyAndSecret)); 
 
-    $data = curl_exec($ch);
-    echo $data;
+                $data = curl_exec($ch);
+                echo $data;
 
-    curl_close($ch); 
+                curl_close($ch);
+
+                // $data = "Datos de prueba";
+
+                return $data;
+
+            } catch (Exception $e) {
+                echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+            }
+        }
+    }
+
+    if (isset($_GET['code'])) {
+        $token = new Token();
+        $data = $token->getData();
+
+        echo $data;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -19,9 +67,9 @@
         Creando la reunión en Zoom...
     </div>
 
-    <div data-request-data="<?php echo $data; ?>"></div>
+    <div id="data-container" data-request-data="<?php echo $data; ?>"></div>
+
     <script>
-        
         debugger
     </script>
     <!-- <script src="cm.js"></script> -->
